0c7024a03ae6466664106509072ce3b63ea9979f
Joshua <44485177+joshuamatthewhagen@users.noreply.github.com>
2021-09-16 00:23:30 -0500

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,13 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#if defined(__linux__) || defined(__linux)
+#if defined(__linux__) || defined(__linux) || defined(__unix__)
 #define JZ_SYS_UNIX
-#endif
+#elif defined(WIN32) || defined(_WIN32)
+#define JZ_SYS_WIN32
 #endif
 
 #ifdef JZ_SYS_UNIX
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#elif JZ_SYS_WIN32
+#include <Windows.h>
+#endif
 #endif
